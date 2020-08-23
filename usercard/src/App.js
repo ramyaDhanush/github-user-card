8f8405821a38a6f8d6e4aa16dc35d0dbf8da648f
RamyaDhanush <ramyadhanush99@gmail.com>
2020-08-23 15:04:57 +0530

    Add : Functionality of user card
    Signed-off-by: RamyaDhanush <ramyadhanush99@gmail.com>

--- usercard/src/App.js
+++ usercard/src/App.js
@@ -1,18 +1,49 @@
-import React from 'react';
+// import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Card from './components/Card';
+// import Card from './components/Card';
+import CardList from './components/CardList'
+import Form from './components/Form'
 
-function App() {
-  return (
-    <div className="App">
-      <div className="header">The GitHub Cards App</div>
-      <Card/>
-    </div>
-  );
+import React, { Component } from 'react'
+
+
+class App extends Component {
+
+  state = {
+    profiles: [],
+  };
+  addNewProfile = (profileData) => {
+    this.setState(prevState => ({
+      profiles: [...prevState.profiles, profileData],
+    }));
+  };
+  render() {
+    return (
+      <div className="App">
+        <div className="header">The GitHub Cards App</div>
+        <div className="body">
+          <Form onSubmit={this.addNewProfile} />
+          <CardList profiles={this.state.profiles} />
+        </div>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default App
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <div className="header">The GitHub Cards App</div>
+//       <Card/>
+//     </div>
+//   );
+// }
+
+// export default App;
 // const App = ({ title }) => (
 //   <div className="header">{title}</div>
 // );
@@ -21,3 +52,23 @@ export default App;
 //   <App title="The GitHub Cards App" />,
 //   mountNode,
 // );
+
+
+
+// state = {
+//   profiles: [],
+// };
+// addNewProfile = (profileData) => {
+//   this.setState(prevState => ({
+//     profiles: [...prevState.profiles, profileData],
+//   }));
+// };
+// render() {
+//   return (
+//     <div>
+//       <div className="header">{this.props.title}</div>
+//       <Form onSubmit={this.addNewProfile} />
+//       <CardList profiles={this.state.profiles} />
+//     </div>
+//   );
+// }	
--- usercard/src/components/Card.js
+++ usercard/src/components/Card.js
@@ -2,15 +2,16 @@ import React, { Component } from 'react'
 
 class Card extends Component {
   render() {
+    const profile = this.props;
     return (
       <div className="github-profile">
-        <img src="https://placehold.it/75" />
+        <img src={profile.avatar_url} />
         <div className="info">
-          <div className="name">Name here...</div>
-          <div className="company">Company here...</div>
+          <div className="name">{profile.name}</div>
+          <div className="company">{profile.company}</div>
         </div>
       </div>
-    )
+    );
   }
 }
 
